01609987777201c7d7c8f437983da94e1f9647c4
Holly Nguyen <71011094+holly-anhnguyen@users.noreply.github.com>
2022-09-22 23:12:41 -0600

    Add files via upload

--- A1.py
+++ A1.py
@@ -1,72 +1,110 @@
-#CHANGE
 # START OF ALL FUNCTIONS
 
 # FUNCTION: Generate password from info cat got from player
 def generateCode(name,location,birth):
 	code=str(name)[::-1]+str(birth)+str(location)
 	return code
-# Remove comment to see example of password
 # print(generateCode('holly','edmonton',2000))
 
-# # FUNCTION: Reaction when player talk/fee/pat cat
-# def cat_reaction(code,times):
-# 	if code == 1: #Talk
-# 		if(times<2):
-# 			print('The cat seems interested. Talk to the cat more')
-# 		else:
-# 			print('Cat has something for you, talk to her more')
-# 	if code==2: #Feed
-# 		if(times<2):
-# 			print('The cat seems still hungry. Feed the cat more')				
-# 		else:
-# 			print('The cat is full now. The cat dont want food anymore')
-# 	if code==3: #Pat		
-# 		if(times<2):
-# 			print('The cat show its belly now. Pat the cat more')				
-# 		else:
-# 			print('Cat seems has enough. Try something else with the cat')	
+# FUNCTION: Reaction when player talk/fee/pat cat
+def cat_reaction(code,times):
+	if code == 1: #Talk
+		if(times<2):
+			print('The cat seems interested. Talk to the cat more')
+		else:
+			print('Cat has something for you, talk to her more')
+	if code==2: #Feed
+		if(times<2):
+			print('The cat seems still hungry. Feed the cat more')				
+		else:
+			print('The cat is full now. The cat dont want food anymore')
+	if code==3: #Pat		
+		if(times<2):
+			print('The cat show its belly now. Pat the cat more')				
+		else:
+			print('Cat seems has enough. Try something else with the cat')	
 
-# # START OF ALL FUNCTIONS
+# START OF ALL FUNCTIONS
 
-# print('You have 1 new message from Andy:')
-# print('Andy: Hey, what\'s up? Do you remember the Tamagotchi game we used to play has just release a desktop version.You have to check it out!')
-# response=input('Sooooo, wanna play? Y/N').upper()
-# while (response!='Y'):
-# 	if response == 'N':
-# 		response=input('Oh come on, it would be super fun. It\'s weekend though. Let\'s give it a try. Y/N').upper()
-# 	else:
-# 		response=input('Nah, give me a proper answer. Type Y for Yes, N for No').upper()	
+print('You have 1 new message from Andy:')
+print('Andy: Hey, what\'s up? Do you remember the Tamagotchi game we used to play has just release a desktop version.You have to check it out!')
+response=input('Sooooo, wanna play? Y/N').upper()
+while (response!='Y'):
+	if response == 'N':
+		response=input('Oh come on, it would be super fun. It\'s weekend though. Let\'s give it a try. Y/N').upper()
+	else:
+		response=input('Nah, give me a proper answer. Type Y for Yes, N for No').upper()	
 
-# # Play type yes
-# print('Tamagotchi - Save a cat is downloading....')
-# print('.....')
-# print('Completed')
-# print('Getting started...')
-# response=input('Type "Meow" to continue').upper()
+# Play type yes
+print('Tamagotchi - Save a cat is downloading....')
+print('.....')
+print('Completed')
+print('Getting started...')
+response=input('Type "Meow" to continue').upper()
 	
-# while response!='MEOW':
-# 	response = input('Oops, Cat does not anything other than cat language. Type "Meow" to continue').upper()
+while response!='MEOW':
+	response = input('Oops, Cat does not anything other than cat language. Type "Meow" to continue').upper()
 
-# # 1-Talk 2-Feed 3-Pat
-# talk=0
-# feed=0
-# pat=0
+# 1-Talk 2-Feed 3-Pat
+talk=0
+feed=0
+pat=0
 
-# while talk<3:
-# 	response = input("What do you want to do with the cat? 1:Talk 2:Feed 3:Pat")
-# 	response=int(response)
-# 	if(response==1):
-# 		talk=talk+1
-# 		print('Talk more')
-# 	elif response==2:
-# 		cat_reaction(response,feed)
-# 		feed=feed+1
-# 	elif response==3:
-# 		cat_reaction(response,pat)
-# 		pat=pat+1
-# 	else:	
-# 		print('Not go too far')
+while talk<3:
+	response = input("What do you want to do with the cat? 1:Talk 2:Feed 3:Pat")
+	response=int(response)
+	if(response==1):
+		talk=talk+1
+		print('Talk more')
+	elif response==2:
+		cat_reaction(response,feed)
+		feed=feed+1
+	elif response==3:
+		cat_reaction(response,pat)
+		pat=pat+1
+	else:	
+		print('Not go too far. You can select 1:Talk 2:Feed 3:Pat')
 
-# # Yirou part
+# Talk > 3 : Yirou part
+# Yirouuuuuuuuuuuuuuuuuu
+# After cat Yirou collect enough data
 
-# 	
+# Generate password from what cat collected
+password = generateCode('holly','edmonton',2000)
+print('IT\'S A TRAP! The cat has just put a spell on you. You are now a cat. You\'ll be stucking in the game forever UNLESS you can enter the right password.')
+response=input('Tell me what\'s the password:')
+pass_count=0
+while(response!=password and pass_count<=5):
+	if pass_count>1:
+		print('Cat: Sorry Edward White, I have no choice, I need someone to carry the spell. You are such a good candidate muahhhh. You know what, I\'m not really that evil. Let me give you a hint. POEM.')
+		print('Oh I forget, if you don\'t get the code right after 5 attempts, you will be stuck here as a cat. After that, the only way you can escape the game is inviting someone to play the game to carry the curse for you.') 
+	else: #from 2 to 5
+		print('Muahhh, still WRONG! You have', 5-pass_count, 'times left.')	
+		response=input('What\'s the code now?')
+	pass_count=pass_count+1	
+
+# Exit the while means either players guess right OR it passed 5 times with wrong password
+if response==password: #Player win
+	print('Oh shoot, You win! Because you are a winner, you can choose a prize. By any chance, do you want to have a cat as a prize? It\'s a real cat, another one, not a trapped one, I promise.')
+	response=input('Type yes (Y) or no (N)').upper()
+	
+	# Ask until player give the proper answer
+	while(response!='Y' or response=='YES' or response!='N' or response!='NO'):
+		response=input('Hey hey, keep it short! Just tell me yes (Y) or no (N)')
+	
+	if response=='Y' or response=='YES':
+		print('Here you are. Take a good care of the cat. And <Input name> walk out of the game holding the new cat. They live happily forever after that. The end!')
+	else 
+		print('Okay, that\'s fine. Hope the next winner will want the cat. Here you go! Good play. And <Input name> walk out of the game.The end!')
+		
+else: #wrong password and exit above while because count>5
+	print('Say Meow! You are trapped as cat now. The only way for you is to invite your friend to play the game. If they got into the trap, you\'ll be free. So what do you choose?')
+	response=str(input('1 - Be a cat, 2 - Send message to your friend'))
+	while(response!=1 or response!=2):
+		response = input('Don\'t try to be smart here. You only have 2 options: 1 or 2. Select properly.')
+	if response==1:
+		print('Such an angel! Happy cat life. The end!')
+	else
+		print('Message sent! Now it\'s time to wait for the target. To be continued..')
+		
+
